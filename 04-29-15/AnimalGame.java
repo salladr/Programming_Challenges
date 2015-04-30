2b1043ab789f7ac44cd90e905d8fa22d9aca5270
Dallas Richardson <dmsricha@uwaterloo.ca>
2015-04-29 21:02:02 -0400

    finished game. added some animals to the tree.

--- 04-29-15/AnimalGame.java
+++ 04-29-15/AnimalGame.java
@@ -11,14 +11,42 @@ public class AnimalGame {
 		Scanner in = new Scanner( System.in );
 		String answer;
 
-		while ( true ) {
-			System.out.println( "Welcome to Animal Guess. Please think of an Animal and type 'y' to proceed -->" );
-			answer = in.nextLine();
-			System.out.println( node.value );
+		System.out.print( "Welcome to Animal Guess. Please think of an Animal and press enter to proceed " );
+		answer = in.nextLine();
+		System.out.println();
 
-			game.saveTree( root );
-			break;
+		while ( node.yes != null  ) {
+			System.out.print( node.value );
+			System.out.print( " --> " );
+			answer = in.nextLine();
+			if ( Character.toLowerCase( answer.charAt(0) ) == 'y' ) {
+				node = node.yes;
+			} else {
+				node = node.no;
+			}
 		}
+
+		System.out.print( "I think your animal is a " + node.value + ". Was I right? --> " );
+		answer = in.nextLine();
+		System.out.println();
+		if ( Character.toLowerCase( answer.charAt(0) ) == 'y' ) {
+			System.out.println( "It is okay to feel bad you did not stump me. I am a computer. :)" );
+			System.out.println( "Thanks for playing!" );
+		} else {
+			System.out.println( "Oh well. please help me learn." );
+			System.out.print( "What is the name of your animal? --> " );
+			String animal = in.nextLine();
+			System.out.print( "What is a unique question that answers yes for " + animal + "? --> " );
+			String question = in.nextLine();
+			System.out.println();
+			System.out.println( "Thank you for teaching me." );
+
+			node.no = new AnimalNode( node.value );
+			node.yes = new AnimalNode( animal );
+			node.value = question;
+		}
+
+		game.saveTree( root );
 	}
 
 	public AnimalNode loadTree() {
